ff7e7bdb78cea7f8d80b93d59c17640eda74a927
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2020-02-26 19:17:49 +0100

    Second exercise done

---
+++ MP3 Programacion A/Extra/PPT1/E2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace E2
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int total = 0;
+
+            Console.WriteLine("Escribe 10 digitos");
+
+            // Iniciar el loop
+            for (int i = 0; i < 11; i++)
+            {
+                total += int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("\nSuma total: " + total + "\nMedia total: " + (total / 10));
+        }
+    }
+}
