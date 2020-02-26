0bb5f4979331e96620ffadc38ee7fc43c4fd306d
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2020-02-26 19:34:25 +0100

    Last exercise, done

---
+++ MP3 Programacion A/Extra/PPT1/E4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace E4
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Escribe dos digitos");
+
+            // Leer valores
+            int integer1 = int.Parse(Console.ReadLine());
+            int integer2 = int.Parse(Console.ReadLine());
+
+            // Lo se, muy largo y poco legible en monitores pequeños
+            Console.WriteLine("Suma: " + (integer1 + integer2) + "\nResta: " + (integer1 - integer2) + "\nMultiplicacion: " + (integer1 * integer2) + "\nDivision: " + ((double)integer1 / (double)integer2));
+        }
+    }
+}
