8171a1518ed95fa13ce49c9ee54b307be3f3a826
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2020-02-26 19:10:36 +0100

    First exercise done

---
+++ MP3 Programacion A/Extra/PPT1/E1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace E1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int total = 0;
+
+            // Iniciar el loop
+            for (int i = 0; i < 11; i++)
+            {
+                total += i;
+                Console.WriteLine("Paso numero " + i + ", valor: " + total);
+            }
+
+            Console.WriteLine("\nTotal: " + total);
+        }
+    }
+}
