c676f484702fd7adee978cf169f06c6d40f82695
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2020-02-26 18:58:56 +0100

    First contact with CSharp

---
+++ MP3 Programacion A/Extra/VideoTutoExtra/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace VideoTutoExtra
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int respuesta;
+
+            Console.WriteLine("Introduce datos!");
+
+            // respuesta = int.Parse(Console.ReadLine());
+
+            // Conversion explicita
+            respuesta = Convert.ToInt32(Console.ReadLine());
+
+            // Conversion implicita seria usar un double con un int asi
+            // double a = (int)respuesta
+
+            Console.WriteLine(respuesta);
+        }
+    }
+}
