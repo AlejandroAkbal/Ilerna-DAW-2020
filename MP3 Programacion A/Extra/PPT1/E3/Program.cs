17c699f63bb35917ea2239e51318b0ae35af75a4
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2020-02-26 19:24:23 +0100

    Exercise 3 done

---
+++ MP3 Programacion A/Extra/PPT1/E3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace E3
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Iniciar el loop
+            for (int i = 0; i < 100; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    // Nothing
+                }
+                else
+                {
+                    Console.WriteLine("Numero impar: " + i);
+                };
+            }
+        }
+    }
+}
