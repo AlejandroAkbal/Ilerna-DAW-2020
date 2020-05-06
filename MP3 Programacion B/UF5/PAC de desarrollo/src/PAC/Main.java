22326feab8f466b007e626a20884b595ba4f5c4e
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2020-05-06 20:45:10 +0200

    Clase MAIN medio completada

--- MP3 Programacion B/UF5/PAC de desarrollo/src/PAC/Main.java
+++ MP3 Programacion B/UF5/PAC de desarrollo/src/PAC/Main.java
@@ -1,8 +1,28 @@
 package PAC;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        Usuario usuario = new Usuario();
+
+        System.out.println("Por favor ingrese su nombre");
+        usuario.setNombre(scanner.next());
+
+        System.out.println("Por favor ingrese su edad");
+        usuario.setEdad(scanner.nextInt());
+
+        do {
+            System.out.println("Por favor ingrese su DNI");
+        } while (!usuario.setDNI(scanner.next()));
+
+        System.out.println(usuario.toString());
+
+        System.out.println("Fin del programa.\nGracias por utilizar la aplicación.");
 
     }
 }
