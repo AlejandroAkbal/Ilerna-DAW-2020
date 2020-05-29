398bb7bb7174f8ce31edcc5540c3584a8eadc52d
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2020-05-29 19:28:37 +0200

    Eliminada repeticion de descripcion ya que lo hereda de la clase Dinero

--- MP3 Programacion B/UF5/PAC de desarrollo/src/Gasto.java
+++ MP3 Programacion B/UF5/PAC de desarrollo/src/Gasto.java
@@ -1,17 +1,14 @@
 public class Gasto extends Dinero {
     private double gasto;
-    private String description;
 
-    public Gasto(double gasto, String description) {
+    public Gasto(double gasto) {
         this.gasto = gasto;
-        this.description = description;
     }
 
     @Override
     public String toString() {
         return "Gasto{" +
                 "gasto=" + gasto +
-                ", description='" + description + '\'' +
                 '}';
     }
 }
--- MP3 Programacion B/UF5/PAC de desarrollo/src/Ingreso.java
+++ MP3 Programacion B/UF5/PAC de desarrollo/src/Ingreso.java
@@ -1,17 +1,14 @@
 public class Ingreso extends Dinero {
     private double ingreso;
-    private String description;
 
-    public Ingreso(double ingreso, String description) {
+    public Ingreso(double ingreso) {
         this.ingreso = ingreso;
-        this.description = description;
     }
 
     @Override
     public String toString() {
         return "Ingreso{" +
                 "ingreso=" + ingreso +
-                ", description='" + description + '\'' +
                 '}';
     }
 }
