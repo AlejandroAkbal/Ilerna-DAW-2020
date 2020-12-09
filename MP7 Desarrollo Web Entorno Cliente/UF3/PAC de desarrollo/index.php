7a16ce07bce652fa7239addd356546088f4bc15a
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2020-12-09 12:29:06 +0100

    feat: add index page for testing

---
+++ MP7 Desarrollo Web Entorno Cliente/UF3/PAC de desarrollo/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Actividad de desarrollo (UF3)</title>
+</head>
+
+<body>
+
+    <h1>Actividad de desarrollo (UF3) </h1>
+
+    <?php
+    $con = include(dirname(__DIR__) . "/PAC de desarrollo/php/conexion.php");
+    ?>
+
+</body>
+
+</html>
