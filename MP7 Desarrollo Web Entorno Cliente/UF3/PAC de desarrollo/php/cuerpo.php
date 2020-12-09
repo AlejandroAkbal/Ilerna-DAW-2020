329e16d23e19cb76c0a942713bb457eff67b42c4
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2020-12-09 12:28:51 +0100

    feat: add ejercicio 3

---
+++ MP7 Desarrollo Web Entorno Cliente/UF3/PAC de desarrollo/php/cuerpo.php
@@ -0,0 +1,42 @@
+<?php
+
+$dbconnect = require('conexion.php');
+
+$sql = "SELECT * FROM noticias ORDER BY hora_creacion DESC LIMIT 5;";
+
+$query = mysqli_query($dbconnect, $sql);
+
+if (!$query) {
+    die(mysqli_error($dbconnect));
+}
+
+if (!(mysqli_num_rows($query) > 0)) {
+    die("No records matching your query were found.");
+}
+
+echo
+    "<table>
+        <thead>
+            <tr>
+                <th>Titulo</th>
+                <th>Contenido</th>
+                <th>Autor</th>
+                <th>Fecha</th>
+                <th>Likes</th>
+            </tr>
+        </thead>
+        <tbody>";
+
+while ($row = mysqli_fetch_array($query)) {
+    echo
+        "   <tr>
+                <td>{$row['titulo']}</td>
+                <td>{$row['contenido']}</td>
+                <td>{$row['autor']}</td>
+                <td>{$row['hora_creacion']}</td>
+                <td>{$row['likes']}</td>
+            </tr>";
+}
+
+echo "  </tbody>
+    </table>";
