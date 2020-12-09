ff3c603ea5a63a8446bee5f109d23b23ef95e2a6
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2020-12-09 20:35:44 +0100

    feat: add more db data

--- MP7 Desarrollo Web Entorno Cliente/UF3/PAC de desarrollo/db/setupscripts.sql
+++ MP7 Desarrollo Web Entorno Cliente/UF3/PAC de desarrollo/db/setupscripts.sql
@@ -31,6 +31,18 @@ CREATE TABLE IF NOT EXISTS noticias (
 
 -- Datos --
 
+INSERT INTO usuarios(nombre, contraseña, edad, fecha_nacimiento, direccion, codigo_postal, provincia, genero) VALUES (
+	'Juan', 'password', 20, '2000-09-12', 'Las gambas, 5, Teruel', 69420, 'Teruel', 'macho'
+)
+
+INSERT INTO usuarios(nombre, contraseña, edad, fecha_nacimiento, direccion, codigo_postal, provincia, genero) VALUES (
+	'Juan y medio', 'password', 20, '2000-09-10', 'Las gambas, 7, Teruel', 69423, 'Teruel', 'macho'
+)
+
+INSERT INTO usuarios(nombre, contraseña, edad, fecha_nacimiento, direccion, codigo_postal, provincia, genero) VALUES (
+	'Juan y dos cuartos', 'password', 20, '2000-09-11', 'Las gambas, 15, Teruel', 69421, 'Teruel', 'hembra'
+)
+
 INSERT INTO noticias(titulo, contenido, autor, likes) VALUES (
 	'Como comer gambas al ajillo', 'Pues muy buena pregunta mi compadre', 'Juan', 5
 )
