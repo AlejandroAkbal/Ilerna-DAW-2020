72bd23dec3458dc4fe11ebf0c084367b141a53d5
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2020-12-09 12:41:14 +0100

    feat: add ejercicio 8

---
+++ MP7 Desarrollo Web Entorno Cliente/UF3/PAC de desarrollo/php/cabecera.php
@@ -0,0 +1,28 @@
+<nav class="navbar">
+    <ul>
+        <li>
+            <a href="/cuerpo.php">Inicio</a>
+        </li>
+        <li>
+            <a href="/list_usuarios.php">Usuarios</a>
+        </li>
+        <li>
+            <a href="/list_noticias.php">Noticias</a>
+        </li>
+        <li>
+            <a href="/form_usuario.php">Crear usuario</a>
+        </li>
+        <li>
+            <a href="/form_noticia.php">Crear noticia</a>
+        </li>
+    </ul>
+</nav>
+
+<style>
+    .navbar>ul {
+        list-style-type: none;
+
+        display: flex;
+        gap: 10px;
+    }
+</style>
