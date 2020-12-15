145ef93098f98f2f0ef5adb006cd49a1c974f0fb
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2020-12-15 16:07:33 +0100

    refactor: index redirects to cuerpo.php

--- MP7 Desarrollo Web Entorno Cliente/UF3/PAC de desarrollo/index.php
+++ MP7 Desarrollo Web Entorno Cliente/UF3/PAC de desarrollo/index.php
@@ -1,9 +1,4 @@
 <?php
-    $title = "Actividad de desarrollo (UF3)";
-
-    require_once(dirname(__DIR__) . "/PAC de desarrollo/php/html_top.php");
-?>
-
-<?php
-    require_once(dirname(__DIR__) . "/PAC de desarrollo/php/html_bottom.php");
+    header("Location: /php/cuerpo.php", true, 301);
+    die();
 ?>
