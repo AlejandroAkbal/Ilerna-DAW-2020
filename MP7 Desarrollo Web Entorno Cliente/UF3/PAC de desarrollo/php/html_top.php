857211c267ab94527c6c978b12704fa060ab0e4a
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2020-12-15 16:01:09 +0100

    feat: create html template

---
+++ MP7 Desarrollo Web Entorno Cliente/UF3/PAC de desarrollo/php/html_top.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>
+            <?php
+            echo $title
+            ?>
+        </title>
+    </head>
+    <body>
+        <?php
+            require_once("cabecera.php");
+        ?>
+
+        <main>
